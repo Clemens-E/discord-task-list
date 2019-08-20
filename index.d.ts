9c70f1f9c218d66b8969ad14510dbaceb3239c49
Clemens Ertle <clemensertle@gmail.com>
2019-08-20 22:37:31 +0200

    Removing dependencies

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,12 @@
-import { Message } from "discord.js";
-
 declare module 'discord-task-list'
 {
     export = MessageTasks;
     class MessageTasks {
         constructor(loading_emoji?: string, done_emoji?: string);
-        newProgress(tasks: string[], msg: Message): Task;
+        newProgress(tasks: string[], msg: any): Task;
+    }
+    type Task = {
+        next(): Promise<any>;
+        start(): Promise<any>;
     }
 }
-
-declare type Task = {
-    next(): Promise<Message>;
-    start(): Promise<Message>;
-}
-
-
